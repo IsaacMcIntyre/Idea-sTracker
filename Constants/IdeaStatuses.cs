248b75147dde57e9ede7113c9964fda10cf54624
George Taylor <george.taylor@and.digital>
2019-09-26 10:55:44 +0100

    Status logic updated

--- Constants/IdeaStatuses.cs
+++ Constants/IdeaStatuses.cs
@@ -7,6 +7,7 @@ namespace IdeasTracker.Constants
         public const string IdeaAccepted = "Idea Accepted";
         public const string PoAssigned = "PO Assigned";
         public const string BootcampAssigned = "Bootcamp Assigned";
+        public const string BootcampReady = "Bootcamp Ready";
         public const string InBootcamp = "In Bootcamp";
         public const string ProjectAdoptable = "Project Adoptable";
         public const string AdoptionRequest = "Adoption Request";
